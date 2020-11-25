8c2d9637aa6f98914499a3f4bbff5c03c11ec52b
Andrew <a4tonetak@gmail.com>
2020-11-26 00:59:27 +0300

    added ended

--- src/com/company/GornerTableModel.java
+++ src/com/company/GornerTableModel.java
@@ -1,63 +1,54 @@
 package com.company;
-
 import javax.swing.table.AbstractTableModel;
 
-public class GornerTableModel extends AbstractTableModel
-{
+@SuppressWarnings("serial")
+
+public class GornerTableModel extends AbstractTableModel {
     private Double[] coefficients;
     private Double from;
     private Double to;
     private Double step;
-    public GornerTableModel(Double from, Double to, Double step, Double[] coefficients)
-    {
+    public GornerTableModel(Double from, Double to, Double step, Double[] coefficients) {
         this.from = from;
         this.to = to;
         this.step = step;
         this.coefficients = coefficients;
     }
-    public Double getFrom()
-    {
+    public Double getFrom() {
         return from;
     }
-    public Double getTo()
-    {
+    public Double getTo() {
         return to;
     }
-    public Double getStep()
-    {
+    public Double getStep() {
         return step;
     }
-    public int getColumnCount()
-    {
-        return 3;           //  3 столбца
+    public int getColumnCount() {
+// В данной модели 3 столбца
+        return 3;
     }
-    public int getRowCount()
-    {
-        return new Double(Math.ceil((to-from)/step)).intValue()+1;// Вычислить количество точек между началом и концом отрезка
-                                                                    // исходя из шага табулирования
+    public int getRowCount() {
+// Вычислить количество точек между началом и концом отрезка
+// исходя из шага табулирования
+        return new Double(Math.ceil((to-from)/step)).intValue()+1;//??
     }
 
-    public Object getValueAt(int row, int col)
-    {
+    public Object getValueAt(int row, int col) {
 // Вычислить значение X как НАЧАЛО_ОТРЕЗКА + ШАГ*НОМЕР_СТРОКИ
         double x = from + step * row;
         Double result;
-        if (col == 0)
-        {
+        if (col == 0) {
 // Если запрашивается значение 1-го столбца, то это X
             return x;
         } else {
 // Если запрашивается значение 2-го столбца, то это значение многочлена
-            if (col == 1)
-            {
+            if (col == 1) {
                 result = coefficients[0];
-                for (int i = 0; i < coefficients.length - 1; i++)
-                {
+                for (int i = 0; i < coefficients.length - 1; i++) {
                     result = result * x + coefficients[i + 1];
                 }
                 return result;
-            } else
-                {
+            } else {
 // 3-й столбец
                 result = coefficients[0];
                 Boolean resultBoolean;
@@ -72,17 +63,15 @@ public class GornerTableModel extends AbstractTableModel
                 {
                     resultBoolean = false;
                 } else if((arr[0])==Math.pow(Math.sqrt(arr[0]), 2))
-                    {
-                         resultBoolean = true;
-                    } else return false;
+                {
+                    resultBoolean = true;
+                } else return false;
                 return resultBoolean;
             }
         }
     }
-    public String getColumnName(int col)
-    {
-        switch (col)
-        {
+    public String getColumnName(int col) {
+        switch (col) {
             case 0:
 // Название 1-го столбца
                 return "Значение X";
@@ -91,7 +80,7 @@ public class GornerTableModel extends AbstractTableModel
                 return "Значение многочлена";
             default:
 // Название 3-го столбца
-                return "Целая часть является квадратом?";
+                return "Малое ли число";
         }
     }
     public Class<?> getColumnClass(int col) {
